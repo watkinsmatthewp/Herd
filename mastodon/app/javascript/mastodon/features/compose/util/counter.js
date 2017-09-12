592d4e93b3aca433359d8fca44b5b97f1b576a6c
Thomas Ortiz <tdortiz@ncsu.edu>
2017-09-12 11:26:03 -0400

    Remove mastodon code

--- mastodon/app/javascript/mastodon/features/compose/util/counter.js
+++
@@ -1,7 +0,0 @@
-const urlPlaceholder = 'xxxxxxxxxxxxxxxxxxxxxxx';
-
-export function countableText(inputText) {
-  return inputText
-    .replace(/https?:\/\/\S+/g, urlPlaceholder)
-    .replace(/(?:^|[^\/\w])@(([a-z0-9_]+)@[a-z0-9\.\-]+)/ig, '@$2');
-};
