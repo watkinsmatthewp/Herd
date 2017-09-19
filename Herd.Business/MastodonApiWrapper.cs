7c20fb5389130b9f9eb62cf18729b7e276842577
Matthew Watkins <watkins.matthewp+github@gmail.com>
2017-09-19 11:27:44 -0400

    Split auth into separate partial class

--- Herd.Business/MastodonApiWrapper.cs
+++ Herd.Business/MastodonApiWrapper.cs
@@ -11,18 +11,12 @@ using System.Threading.Tasks;
 
 namespace Herd.Business
 {
-
-    public class MastodonApiWrapper : IMastodonApiWrapper
+    public partial class MastodonApiWrapper : IMastodonApiWrapper
     {
-        private const Scope ALL_SCOPES = Scope.Read | Scope.Write | Scope.Follow;
-
         #region Private member variables
 
-        private Lazy<AuthenticationClient> _authClient = null;
-        private Lazy<MastodonClient> _mastodonClient = null;
-
-        private AuthenticationClient AuthClient => _authClient.Value;
-        private MastodonClient ApiClient => _mastodonClient.Value;
+        private Lazy<MastodonClient> _apiClient = null;
+        private MastodonClient ApiClient => _apiClient.Value;
 
         #endregion
 
@@ -55,56 +49,7 @@ namespace Herd.Business
             UserApiToken = userApiToken;
 
             _authClient = new Lazy<AuthenticationClient>(LoadAuthenticationClient);
-            _mastodonClient = new Lazy<MastodonClient>(LoadMastodonClient);
-        }
-
-        #endregion
-
-        #region Public methods
-
-        public async Task<HerdAppRegistrationDataModel> RegisterApp()
-        {
-            var appRegistration = await AuthClient.CreateApp("Herd", ALL_SCOPES);
-            return new HerdAppRegistrationDataModel
-            {
-                ClientId = appRegistration.ClientId,
-                ClientSecret = appRegistration.ClientSecret,
-                RegistrationID = appRegistration.Id,
-                Instance = appRegistration.Instance
-            };
-        }
-
-        public async Task<Account> GetUserAccount() => await ApiClient.GetCurrentUser();
-
-        public string GetOAuthUrl(string redirectURL) => AuthClient.OAuthUrl(redirectURL);
-
-        #endregion
-
-        #region Private helper methods
-
-        private AuthenticationClient LoadAuthenticationClient()
-        {
-            return AppRegistration == null ? new AuthenticationClient(MastodonHostInstance) : new AuthenticationClient(GetAppRegistration());
-        }
-
-        private MastodonClient LoadMastodonClient()
-        {
-            return new MastodonClient(GetAppRegistration(), new Auth
-            {
-                AccessToken = UserApiToken
-            });
-        }
-
-        private AppRegistration GetAppRegistration()
-        {
-            return new AppRegistration
-            {
-                ClientId = AppRegistration.ClientId,
-                ClientSecret = AppRegistration.ClientSecret,
-                Id = AppRegistration.RegistrationID,
-                Instance = AppRegistration.Instance,
-                Scope = ALL_SCOPES
-            };
+            _apiClient = new Lazy<MastodonClient>(LoadMastodonClient);
         }
 
         #endregion
