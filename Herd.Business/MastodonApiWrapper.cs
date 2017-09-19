067a09f3589b17208d07dea19bac9b5afda46b0f
Matthew Watkins <watkins.matthewp+github@gmail.com>
2017-09-19 11:13:28 -0400

    Broke interface into own file

--- Herd.Business/MastodonApiWrapper.cs
+++ Herd.Business/MastodonApiWrapper.cs
@@ -11,16 +11,6 @@ using System.Threading.Tasks;
 
 namespace Herd.Business
 {
-    public interface IMastodonApiWrapper
-    {
-        string MastodonHostInstance { get; }
-        HerdAppRegistrationDataModel AppRegistration { get; set; }
-        string UserApiToken { get; set; }
-
-        Task<HerdAppRegistrationDataModel> RegisterApp();
-        Task<Account> GetUserAccount();
-        string GetOAuthUrl(string redirectURL);
-    }
 
     public class MastodonApiWrapper : IMastodonApiWrapper
     {
