c059f4c7ac5cc7aeff730d079f40ce01523eddf5
Matthew Watkins <watkins.matthewp+github@gmail.com>
2017-09-19 11:30:23 -0400

    Added MastodonFeedApiWrapper

---
+++ Herd.Business/ApiWrappers/MastodonFeedApiWrapper.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Threading.Tasks;
+using Herd.Data.Models;
+using Mastonet;
+using Mastonet.Entities;
+
+namespace Herd.Business
+{
+    public partial class MastodonApiWrapper : IMastodonApiWrapper
+    {
+        
+    }
+}
