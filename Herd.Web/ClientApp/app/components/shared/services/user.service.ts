a834e2e4edda0d68e63a4620b7182901c1c658d1
Thomas Ortiz <tdortiz@ncsu.edu>
2017-09-21 12:57:49 -0400

    Reformatted user service

--- Herd.Web/ClientApp/app/components/shared/services/user.service.ts
+++ Herd.Web/ClientApp/app/components/shared/services/user.service.ts
@@ -16,23 +16,30 @@ export class UserService {
     }
  
     getById(id: number) {
-        return this.http.get('/api/users/' + id, this.jwt()).map((response: Response) => response.json());
+        return this.http.get('/api/users/' + id, this.jwt())
+            .map((response: Response) => response.json())
+            .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
     }
  
     create(user: User) {
-        return this.http.post('/api/users', user, this.jwt()).map((response: Response) => response.json());
+        return this.http.post('/api/users', user, this.jwt())
+            .map((response: Response) => response.json())
+            .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
     }
  
     update(user: User) {
-        return this.http.put('/api/users/' + user.id, user, this.jwt()).map((response: Response) => response.json());
+        return this.http.put('/api/users/' + user.id, user, this.jwt())
+            .map((response: Response) => response.json())
+            .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
     }
  
     delete(id: number) {
-        return this.http.delete('/api/users/' + id, this.jwt()).map((response: Response) => response.json());
+        return this.http.delete('/api/users/' + id, this.jwt())
+            .map((response: Response) => response.json())
+            .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
     }
  
     // private helper methods
- 
     private jwt() {
         // create authorization header with jwt token
         let currentUser = JSON.parse(localStorage.getItem('currentUser') || '');
