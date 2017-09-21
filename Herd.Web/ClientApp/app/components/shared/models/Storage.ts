2457ad1d824ea0dad48da3f73f7ddd4e22102b66
Thomas Ortiz <tdortiz@ncsu.edu>
2017-09-21 02:39:04 -0400

    Added a localStorage accessor object to fix server-side rendering issue
    Because files are being pre-processed on the server we would get an error when attempting to access LocalStorage saying it didn't exist. The only fix to this is to wrap localstorage into a custom object.

---
+++ Herd.Web/ClientApp/app/components/shared/models/Storage.ts
@@ -0,0 +1,28 @@
+﻿export abstract class StorageService {
+    abstract getItem(key: string): any;
+    abstract setItem(key: string, val: any): void;
+    abstract removeItem(key: string): void;
+}
+
+export class BrowserStorage extends StorageService {
+    getItem(key: string) {
+        return localStorage.getItem(key);
+    }
+
+    setItem(key: string, val: any) {
+        localStorage.setItem(key, val);
+    }
+    removeItem(key: string) {
+        localStorage.removeItem(key);
+    }
+}
+
+export class ServerStorage extends StorageService {
+    getItem(key: string) {
+        return '';
+    }
+
+    setItem(key: string, val: any) {}
+
+    removeItem(key: string) {}
+}
