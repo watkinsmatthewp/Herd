9a17ed821c5ed1739279acc98497bc335e2ec2ec
Thomas Ortiz <tdortiz@ncsu.edu>
2017-09-21 02:37:19 -0400

    Added a register component

---
+++ Herd.Web/ClientApp/app/components/register/register.component.ts
@@ -0,0 +1,35 @@
+﻿import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { MastodonService } from '../shared/services/mastodon.service';
+import { AlertService } from '../shared/services/alert.service';
+import { UserService } from '../shared/services/user.service';
+
+@Component({
+    selector: 'register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+    model: any = {};
+    loading = false;
+
+    constructor(private router: Router, private userService: UserService, private alertService: AlertService) { }
+
+    register() {
+        this.loading = true;
+        
+        this.userService.create(this.model)
+            .subscribe(
+            data => {
+                // set success message and pass true paramater to persist the message after redirecting to the login page
+                this.alertService.success('Registration successful', true);
+                this.router.navigate(['/login']);
+            },
+            error => {
+                this.alertService.error(error);
+                this.loading = false;
+            });
+    }
+}
