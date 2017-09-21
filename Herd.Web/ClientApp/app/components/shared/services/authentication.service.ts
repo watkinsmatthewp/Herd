8aeb053786486da5201ed19031b0e20ceaf571c6
Thomas Ortiz <tdortiz@ncsu.edu>
2017-09-21 02:40:37 -0400

    Added initial service work for authentication and users

---
+++ Herd.Web/ClientApp/app/components/shared/services/authentication.service.ts
@@ -0,0 +1,37 @@
+﻿import { Injectable } from '@angular/core';
+import { Http, Headers, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map'
+
+import { StorageService } from '../models/Storage';
+
+@Injectable()
+export class AuthenticationService {
+    constructor(private http: Http, private localStorage: StorageService) { }
+
+    isAuthenticated() {
+        if (this.localStorage.getItem('currentUser')) {
+            return true; //logged in
+        }
+        return false; // not logged in
+    }
+
+    login(username: string, password: string) {
+        return this.http.post('/api/authenticate', JSON.stringify({ username: username, password: password }))
+            .map((response: Response) => {
+                // login successful if there's a jwt token in the response
+                let user = response.json();
+                if (user && user.token) {
+                    // store user details and jwt token in local storage to keep user logged in between page refreshes
+                    localStorage.setItem('currentUser', JSON.stringify(user));
+                }
+
+                return user;
+            });
+    }
+
+    logout() {
+        // remove user from local storage to log user out
+        localStorage.removeItem('currentUser');
+    }
+}
