4db61d917181be24bef086adbcf6abdbb1984f46
jcsc <jcstone3@ncsu.edu>
2017-09-21 14:02:33 -0400

    Created post component for use with localfeed component

---
+++ Herd.Web/ClientApp/app/components/post/post.component.ts
@@ -0,0 +1,12 @@
+﻿
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'post',
+    templateUrl: './post.component.html'
+})
+export class PostComponent {
+    constructor() {
+        
+    }
+}
