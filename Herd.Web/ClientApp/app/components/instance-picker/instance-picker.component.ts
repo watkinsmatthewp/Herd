59b9d0e13b3510d72da0e15de24630b483edab9d
Thomas Ortiz <tdortiz@ncsu.edu>
2017-09-21 02:36:32 -0400

    Added a mastodon instance picker component

---
+++ Herd.Web/ClientApp/app/components/instance-picker/instance-picker.component.ts
@@ -0,0 +1,48 @@
+﻿import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { AlertService } from '../shared/services/alert.service';
+import { AuthenticationService } from '../shared/services/authentication.service';
+
+@Component({
+    selector: 'instance-picker',
+    templateUrl: './instance-picker.component.html',
+    styleUrls: []
+})
+export class InstancePickerComponent {
+    model: any = {};
+    loading = false;
+    oAuthUrl: string;
+    instance: string;
+
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        private authenticationService: AuthenticationService,
+        private alertService: AlertService) { }
+
+    getOAuthToken() {
+        this.loading = true;
+
+        this.oAuthUrl = "https://google.com";
+        /**
+        this.authenticationService.login(this.model.username, this.model.password)
+            .subscribe(
+            data => {
+                this.router.navigate([this.returnUrl]);
+            },
+            error => {
+                this.alertService.error(error);
+                this.loading = false;
+            });
+        */
+        this.loading = false;
+    }
+
+    submitOAuthToken() {
+        this.loading = true;
+        this.alertService.success("Successfully linked with " + this.model.instance, true);
+        this.router.navigate(['/login']); // TODO: this should be home
+    }
+}
