462d93f2440286db4a039ee310367bdd8678b4be
Matthew Watkins <watkins.matthewp+github@gmail.com>
2017-09-21 11:19:22 -0400

    Dummy timeline

--- Herd.Business/ApiWrappers/MastodonFeedApiWrapper.cs
+++ Herd.Business/ApiWrappers/MastodonFeedApiWrapper.cs
@@ -8,6 +8,10 @@ namespace Herd.Business
 {
     public partial class MastodonApiWrapper : IMastodonApiWrapper
     {
-        
+        public async Task<object> DoStuff()
+        {
+            var timeline = await ApiClient.GetHomeTimeline();
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ Herd.Web/Controllers/HerdApi/FeedApiController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Herd.Web.Controllers
+{
+    [Route("api/feed")]
+    public class FeedApiController : BaseApiController
+    {
+        [HttpGet("new_items")]
+        public IActionResult NewItems()
+        {
+            return new ObjectResult(new []
+            {
+                new { text = "Hello world!" },
+                new { text = "Another post" },
+                new { text = "Yet another post" }
+            });
+        }
+    }
+}
