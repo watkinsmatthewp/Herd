44f7512614678857cb7facca6ae1210e355c40ad
Thomas Ortiz <tdortiz@ncsu.edu>
2017-09-21 20:23:34 -0400

    Clean up

--- Herd.Web/ClientApp/app/components/shared/services/user.service.ts
+++ Herd.Web/ClientApp/app/components/shared/services/user.service.ts
@@ -8,44 +8,6 @@ import { User } from '../models/User';
 @Injectable()
 export class UserService {
     constructor(private http: Http) { }
- 
-    getAll() {
-        return this.http.get('/api/users', this.jwt())
-            .map((response: Response) => response.json())
-            .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
-    }
- 
-    getById(id: number) {
-        return this.http.get('/api/users/' + id, this.jwt())
-            .map((response: Response) => response.json())
-            .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
-    }
- 
-    create(user: User) {
-        return this.http.post('/api/users', user, this.jwt())
-            .map((response: Response) => response.json())
-            .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
-    }
- 
-    update(user: User) {
-        return this.http.put('/api/users/' + user.id, user, this.jwt())
-            .map((response: Response) => response.json())
-            .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
-    }
- 
-    delete(id: number) {
-        return this.http.delete('/api/users/' + id, this.jwt())
-            .map((response: Response) => response.json())
-            .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
-    }
- 
-    // private helper methods
-    private jwt() {
-        // create authorization header with jwt token
-        let currentUser = JSON.parse(localStorage.getItem('currentUser') || '');
-        if (currentUser && currentUser.token) {
-            let headers = new Headers({ 'Authorization': 'Bearer ' + currentUser.token });
-            return new RequestOptions({ headers: headers });
-        }
-    }
+
+
 }
