d5ea4c494cacee39362f1a0cb79409ff5350ac17
Thomas Ortiz <tdortiz@ncsu.edu>
2017-09-21 20:23:58 -0400

    If on login the user doesn't have instance redirct to instance-picker

--- Herd.Web/ClientApp/app/components/login/login.component.ts
+++ Herd.Web/ClientApp/app/components/login/login.component.ts
@@ -39,10 +39,10 @@ export class LoginComponent implements OnInit {
         this.authenticationService.login(this.model.email, this.model.password)
         .finally(() => this.loading = false)
         .subscribe(user => {
-            if (user && user.email && user.instance) {
+            if (user && user.email && !user.instance) {
                 this.localStorage.setItem('currentUser', JSON.stringify(user));
                 this.alertService.success("Successfully Logged", true);
-                this.router.navigateByUrl('/home');
+                this.router.navigateByUrl('/instance-picker');
             }
         }, error => {
             this.alertService.error(error);
