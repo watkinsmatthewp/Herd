f3dcb7b0136fb34adaf57fe6cc96618d49c42fc5
Matthew Watkins <mpwatki2@ncsu.edu>
2017-09-25 12:36:24 -0400

    Added logging features

---
+++ Herd.Logging/HerdDefaultLogFormatter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Herd.Logging
+{
+    public class HerdDefaultLogFormatter : IHerdLogFormatter
+    {
+        public string GetLogLine(Guid? id, HerdLogLevel logLevel, string message, IEnumerable<KeyValuePair<string, string>> contextParameters = null)
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("{");
+            sb.AppendLine($"  [ID] = {id}");
+            sb.AppendLine($"  [LEVEL] = {logLevel.ToString().ToUpperInvariant()}");
+            sb.AppendLine($"  [TIME] = {DateTime.UtcNow}");
+            sb.AppendLine($"  [MESSAGE] = {message}");
+
+            if (contextParameters != null)
+            {
+                foreach (var contextParameter in contextParameters)
+                {
+                    sb.AppendLine($"  [{contextParameter.Key}] = {contextParameter.Value}");
+                }
+            }
+
+            sb.AppendLine("}");
+            sb.AppendLine();
+            sb.AppendLine("--------------------------------------------------------------------------------");
+            sb.AppendLine();
+
+            return sb.ToString();
+        }
+    }
+}
+
