b4a75073429c4861b4d7c32c5eec62e70aa14ac4
jcsc <jcstone3@ncsu.edu>
2017-10-02 17:10:41 -0400

    Pulling status text from html to typescript component
    click is registering twice for some reason.

--- Herd.Web/ClientApp/app/components/status-form/status-form.component.ts
+++ Herd.Web/ClientApp/app/components/status-form/status-form.component.ts
@@ -1,19 +1,26 @@
-﻿import { Component } from '@angular/core';
+﻿import { Component, OnInit } from '@angular/core';
+
+
+import { MastodonService } from "../../services";
 
 @Component({
     selector: 'status-form',
     templateUrl: './status-form.component.html',
     styleUrls: ['./status-form.component.css']
 })
-export class StatusFormComponent {
+export class StatusFormComponent implements OnInit {
     maxStatusLength: number = 200;
     model: any = {
         status: ""
     };
 
-    constructor() { }
+    constructor(private mastodonService: MastodonService) { }
 
     submitStatus() {
+        console.log(this.model.status);
+    }
 
+    ngOnInit() {
+        
     }
 }
