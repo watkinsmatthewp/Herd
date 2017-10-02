0f95a9647f8df62c1be39d527c961172a2b65103
jcsc <jcstone3@ncsu.edu>
2017-10-01 21:06:26 -0400

    Figuring out how to test the backend.
    Wrote a simple test.

--- Herd.Data.UnitTests/UnitTest1.cs
+++ Herd.Data.UnitTests/UnitTest1.cs
@@ -1,3 +1,5 @@
+using Herd.Data.Models;
+using Herd.Data.Providers;
 using System;
 using Xunit;
 
@@ -6,9 +8,20 @@ namespace Herd.Data.UnitTests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void TestAppRegistration()
         {
+            HerdFileDataProvider provider = new HerdFileDataProvider();
+            HerdAppRegistrationDataModel registration = new HerdAppRegistrationDataModel();
+            registration.MastodonAppRegistrationID = 1;
+            registration.ClientId = "05";
+            registration.ClientSecret = "sneaky";
+            registration.Instance = "social";
 
+            provider.CreateAppRegistration(registration);
+
+            HerdAppRegistrationDataModel result = provider.GetAppRegistration(registration.ID);
+
+            Assert.Equal(registration.ClientId, result.ClientId);
         }
     }
 }
