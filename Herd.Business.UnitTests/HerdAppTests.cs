5933730db6fd5806b3d5d69539cdf8ca0255c683
jcsc <jcstone3@ncsu.edu>
2017-10-02 15:42:08 -0400

    Added simple mock test for HerdApp.GetOAuthURL

--- Herd.Business.UnitTests/HerdAppTests.cs
+++ Herd.Business.UnitTests/HerdAppTests.cs
@@ -48,5 +48,40 @@ namespace Herd.Business.UnitTests
             Assert.Equal("mastodon.instance", result.Data?.Registration?.Instance);
             Assert.Equal(42, result.Data?.Registration?.MastodonAppRegistrationID);
         }
+
+        [Fact]
+        public void GetOAuthURLTest()
+        {
+
+            // Tell Moq to create an objects that implement the interfaces of the HerdApp dependencies
+            var mockData = new Mock<IHerdDataProvider>();
+            var mockMastodonApiWrapper = new Mock<IMastodonApiWrapper>();
+            var mockLogger = new Mock<IHerdLogger>();
+
+            // Setup the mocks
+            mockData.Setup(d => d.GetAppRegistration(3)).Returns(new HerdAppRegistrationDataModel
+            {
+                ID = 3,
+                ClientId = "client-id",
+                ClientSecret = "client-secret",
+                Instance = "mastodon.instance",
+                MastodonAppRegistrationID = 42
+            });
+
+            mockMastodonApiWrapper.Setup(p => p.GetOAuthUrl("https://SentURL")).Returns("https://ReturnedURL");
+
+            // Create the HerdApp using the mock objects
+            var herdApp = new HerdApp(mockData.Object, mockMastodonApiWrapper.Object, mockLogger.Object);
+
+            // Perform the test
+            
+            var result = herdApp.GetOAuthURL(new HerdAppGetOAuthURLCommand { AppRegistrationID = 3, ReturnURL = "https://SentURL" });
+
+            // Verify the result
+            Assert.True(result?.Success);
+            Assert.Equal("https://ReturnedURL", result.Data?.URL);
+
+        }
     }
 }
+
