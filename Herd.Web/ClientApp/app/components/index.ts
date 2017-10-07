aa438324d078068e8a7dc1fd91522f6314ca8e55
Thomas Ortiz <thomasortiz95@gmail.com>
2017-10-07 19:58:23 -0400

    Added status-modal

--- Herd.Web/ClientApp/app/components/index.ts
+++ Herd.Web/ClientApp/app/components/index.ts
@@ -2,4 +2,5 @@
 export * from './instance-picker/instance-picker.component';
 export * from './navmenu/navmenu.component';
 export * from './status/status.component';
+export * from './status-modal/status-modal.component';
 export * from './status-form/status-form.component';
