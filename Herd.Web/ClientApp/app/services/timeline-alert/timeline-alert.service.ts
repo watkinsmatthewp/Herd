1d0def4a001ffd3a80c910f5c99eff1349bc5f56
Thomas Ortiz <thomasortiz95@gmail.com>
2017-10-10 00:29:48 -0400

    #61 - Added TimelineAlert to notify timeline when status is clicked

---
+++ Herd.Web/ClientApp/app/services/timeline-alert/timeline-alert.service.ts
@@ -0,0 +1,19 @@
+﻿import { Injectable } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Subject } from 'rxjs/Subject';
+
+@Injectable()
+export class TimelineAlertService {
+    private subject = new Subject<any>();
+
+    constructor() {}
+
+    addMessage(message: string, statusId: number) {
+        this.subject.next({ message: message, statusId: statusId });
+    }
+
+    getMessage(): Observable<any> {
+        return this.subject.asObservable();
+    }
+}
