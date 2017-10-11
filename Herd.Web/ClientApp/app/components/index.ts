4c205b60e5003d5a8bde1a2d53eb9b91f0f481bd
jcsc <jcstone3@ncsu.edu>
2017-10-11 17:48:19 -0400

    #65 Added base usercard component and searchresults page.

--- Herd.Web/ClientApp/app/components/index.ts
+++ Herd.Web/ClientApp/app/components/index.ts
@@ -6,3 +6,4 @@ export * from './status-modal/status-modal.component';
 export * from './status-form/status-form.component';
 export * from './status-form/status-form-modal/status-form-modal.component';
 export * from './status-reply-modal/status-reply-modal.component';
+export * from './usercard/usercard.component';
---
+++ Herd.Web/ClientApp/app/components/usercard/usercard.component.ts
@@ -0,0 +1,18 @@
+﻿import { Component, OnInit } from '@angular/core';
+
+import { MastodonService } from "../../services";
+
+@Component({
+    selector: 'usercard',
+    templateUrl: './usercard.component.html',
+    styleUrls: ['./usercard.component.css']
+})
+export class UserCardComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() {
+
+    }
+
+}
