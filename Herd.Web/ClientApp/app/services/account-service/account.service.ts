23cc090da3b1ff2da4cfb98b200981f80a59071e
Thomas Ortiz <thomasortiz95@gmail.com>
2017-10-14 01:12:34 -0400

    #73 - Fixed merge issue

--- Herd.Web/ClientApp/app/services/account-service/account.service.ts
+++ Herd.Web/ClientApp/app/services/account-service/account.service.ts
@@ -1,11 +1,24 @@
 ﻿import { Injectable } from '@angular/core';
-import { Http, Headers, RequestOptions, Response } from '@angular/http';
 import { Observable } from "rxjs/Observable";
 
 import { User } from '../../models';
+import { Account } from '../../models/mastodon';
+import { HttpClientService } from '../http-client-service/http-client.service';
+
 
 @Injectable()
 export class AccountService {
-    constructor(private http: Http) { }
+
+    constructor(private httpClient: HttpClientService) { }
+
+    // Calls Api to get a user by username 
+    getUser(userId: string) {
+        let queryString = "?userID=" + userId;
+        return this.httpClient.get('api/mastodon-users/search' + queryString)
+            .map((response) => {
+                console.log("Response", response);
+                return response.Users[0] as Account;
+            });
+    }
 
 }
