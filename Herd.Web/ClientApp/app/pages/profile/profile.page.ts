af4402927c6adcc8b3478a6bcf320d4a6b6a92cd
Dana Christo <christo.dana@gmail.com>
2017-10-13 21:20:16 -0400

    #73 - Got the profile picture to show up!

--- Herd.Web/ClientApp/app/pages/profile/profile.page.ts
+++ Herd.Web/ClientApp/app/pages/profile/profile.page.ts
@@ -1,10 +1,34 @@
-﻿import { Component } from '@angular/core';
+﻿import { Component, OnInit } from '@angular/core';
+import { NotificationsService } from "angular2-notifications";
+
+import { AccountService } from "../../services";
+import { Account } from '../../models/mastodon';
+import { Storage } from '../../models';
 
 @Component({
     selector: 'profile',
     templateUrl: './profile.page.html',
 })
-export class ProfilePage {
-    constructor() {
+export class ProfilePage implements OnInit {
+    account: Account;
+    loading: boolean = false;
+    constructor(private accountService: AccountService,
+        private localStorage: Storage,
+        private alertService: NotificationsService) {
+    }
+
+    ngOnInit() {
+        this.loading = true;
+        let currentUser = JSON.parse(this.localStorage.getItem('currentUser'));
+        let userId = currentUser.MastodonConnection.MastodonUserID;
+        console.log("mastodonUserID", userId);
+        this.accountService.getUser(userId)
+            .finally(() => this.loading = false)
+            .subscribe(account => {
+                console.log("account", account);
+                this.account = account;
+            }, error => {
+                this.alertService.error("Error", error.error);
+            });
     }
 }
