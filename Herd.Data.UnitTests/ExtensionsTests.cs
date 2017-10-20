fb06ced101db76fbd7e6c2f18d22e81d37754ec5
Thomas Ortiz <tdortiz@ncsu.edu>
2017-10-20 11:56:57 -0400

    Added Data.extensions

---
+++ Herd.Data.UnitTests/ExtensionsTests.cs
@@ -0,0 +1,15 @@
+﻿using Herd.Data.Models;
+using Xunit;
+
+namespace Herd.Data.UnitTests
+{
+    public class ExtensionsTests
+    {
+        [Fact]
+        public void GetEntityNameTest()
+        {
+            var userAccount = new UserAccount();
+            Assert.Equal("UserAccount", userAccount.GetEntityName());
+        }
+    }
+}
