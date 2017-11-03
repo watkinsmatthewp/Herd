1141b16646edb99d55f67a27e4e9f99f7e7a662f
Thomas Ortiz <tdortiz@ncsu.edu>
2017-11-03 03:23:41 -0400

    Initial work for hashtag search results

--- Herd.Web/ClientApp/app/pages/searchresults/searchresults.page.ts
+++ Herd.Web/ClientApp/app/pages/searchresults/searchresults.page.ts
@@ -1,7 +1,7 @@
 ﻿import { Component, OnInit, Input, ViewChild } from '@angular/core';
 
-import { AccountService } from '../../services';
-import { Account } from "../../models/mastodon";
+import { AccountService, StatusService } from '../../services';
+import { Account, Status } from "../../models/mastodon";
 
 import { ActivatedRoute, Router } from '@angular/router';
 import { NotificationsService } from "angular2-notifications";
@@ -12,30 +12,19 @@ import { NotificationsService } from "angular2-notifications";
     styleUrls: ['./searchResults.page.css']
 })
 export class SearchResultsPage implements OnInit {
-    @ViewChild('userWrapper') usersWrapper: any;
+    @ViewChild('usersWrapper') usersWrapper: any;
+    @ViewChild('statusesWrapper') statusesWrapper: any;
 
     search: string;
-    haveSearchResults: boolean = false;
     finishedSearching: boolean = false;
-    userCards: Account[]; // List of users that the search found
+    userCards: Account[] = []; // List of users that the search found
+    statuses: Status[] = [];
+    new_statuses: Status[] = [];
+    loading: boolean = false;
 
     // Keeping  it simple for now
-    constructor(private accountService: AccountService, private route: ActivatedRoute, private router: Router, private toastService: NotificationsService) { }
-
-    performSearch() {
-        this.haveSearchResults = false;
-        this.finishedSearching = false;
-        let progress = this.toastService.info("Searching for", this.search + " ...", { timeOut: 0 })
-        this.accountService.search({ name: this.search, includeFollowedByActiveUser: true, includeFollowsActiveUser: true })
-            .subscribe(users => {
-                this.toastService.remove(progress.id);
-                if (users.length > 0) {
-                    this.haveSearchResults = true;
-                }
-                this.finishedSearching = true;
-                this.userCards = users;
-            });
-    }
+    constructor(private accountService: AccountService, private route: ActivatedRoute, private router: Router,
+                private statusService: StatusService, private toastService: NotificationsService) { }
 
     ngOnInit(): void {
         this.route
@@ -44,8 +33,29 @@ export class SearchResultsPage implements OnInit {
                 this.search = params['searchString'] || "John";
                 this.performSearch();
             });
+        // setInterval(() => { this.checkForNewStatuses(); }, 10 * 1000);
     }
 
+    performSearch() {
+        this.finishedSearching = false;
+        this.getInitialStatuses();
+        this.getInitialUsers();
+    }
+
+    getInitialStatuses() {
+        this.statusService.search({ hashtag: this.search })
+            .subscribe(statuses => {
+                this.statuses = statuses;
+            });
+    }
+
+    getInitialUsers() {
+        this.accountService.search({ name: this.search, includeFollowedByActiveUser: true, includeFollowsActiveUser: true })
+            .subscribe(users => {
+                this.finishedSearching = true;
+                this.userCards = users;
+            });
+    }
 
     getMoreUsers() {
         let lastID = this.userCards[this.userCards.length-1].MastodonUserId;
@@ -57,13 +67,54 @@ export class SearchResultsPage implements OnInit {
             });
     }
 
+
+    checkForNewStatuses() {
+        if (this.statuses.length > 0) {
+            this.statusService.search({ hashtag: this.search, sinceID: this.statuses[0].Id })
+                .finally(() => this.loading = false)
+                .subscribe(newItems => {
+                    this.statuses = newItems;
+                });
+        } else {
+            this.getInitialStatuses();
+        }
+    }
+
+    getPreviousStatuses() {
+        this.loading = true;
+        this.statusService.search({ hashtag: this.search, maxID: this.statuses[this.statuses.length - 1].Id })
+            .finally(() => this.loading = false)
+            .subscribe(new_items => {
+                this.appendItems(this.statuses, new_items);
+                let currentYPosition = this.statusesWrapper.nativeElement.scrollTop;
+                this.statusesWrapper.nativeElement.scrollTo(0, currentYPosition);
+            });
+    }
+
+    /**
+     * Add the new items to main statuses array, scroll to top, empty newItems
+     */
+    viewNewStatuses() {
+        this.prependItems(this.statuses, this.new_statuses);
+        this.scrollToTop('statuses');
+        this.new_statuses = [];
+    }
+
+
+
+
+
+
+
+
     /**
      * Scrolls the status area to the top
      */
     scrollToTop(tab: string) {
         if (tab === 'users')
             this.usersWrapper.nativeElement.scrollTo(0, 0);
-
+        else if (tab === 'statuses')
+            this.statusesWrapper.nativeElement.scrollTo(0, 0);
     }
 
     /**
@@ -74,6 +125,8 @@ export class SearchResultsPage implements OnInit {
     onScrollDown(ev: any, tab: string) {
         if (tab === 'users')
             this.getMoreUsers();
+        else if (tab === 'statuses')
+            this.getPreviousStatuses();
     }
 
     /** Infinite Scrolling Handling */
