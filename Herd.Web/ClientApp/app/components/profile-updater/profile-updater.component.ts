333d999d21d35e3cabbc8cf8bcd910269bbb174e
jcsc <jcstone3@ncsu.edu>
2017-11-11 22:47:43 -0500

    #98 pushing commit that was lost in update
    Created shell for ProfileUpdaterComponent

--- Herd.Web/ClientApp/app/components/profile-updater/profile-updater.component.ts
+++ Herd.Web/ClientApp/app/components/profile-updater/profile-updater.component.ts
@@ -7,10 +7,44 @@
 })
 export class ProfileUpdaterComponent implements OnInit {
 
+    model: any = {
+        display: "",
+    };
+
     constructor() { }
 
     ngOnInit() {
         
+    }
+
+    onHeaderFileChange(event: any) {
+        let fileList: FileList = event.target.files;
+        if (fileList.length > 0) {
+            let file = fileList[0];
+            this.model.headerFile = file;
+            this.model.headerFilename = file.name;
+
+            let reader = new FileReader();
+            reader.onload = (e: any) => {
+                
+            }
+            reader.readAsDataURL(event.target.files[0]);
+        }
+    }
+
+    onAvatarFileChange(event: any) {
+        let fileList: FileList = event.target.files;
+        if (fileList.length > 0) {
+            let file = fileList[0];
+            this.model.avatarFile = file;
+            this.model.avatarFilename = file.name;
+
+            let reader = new FileReader();
+            reader.onload = (e: any) => {
+                
+            }
+            reader.readAsDataURL(event.target.files[0]);
+        }
     }
 
 }
