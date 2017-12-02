6ff72e204d76b31a5664a00ac7b08ceafcdc3159
mpwatki2 <mpwatki2@ncsu.edu>
2017-12-01 22:32:07 -0500

    Update for URLs, etc

--- Herd.Web/Program.cs
+++ Herd.Web/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using System;
 
 namespace Herd.Web
 {
@@ -7,12 +8,17 @@ namespace Herd.Web
     {
         public static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                throw new ArgumentException("Usage => dotnet run -- urls=http://localhost;http://url-1.com;http://url-2.com");
+            }
             BuildWebHost(args).Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls(args[0].Split("=")[1])
                 .Build();
     }
 }
